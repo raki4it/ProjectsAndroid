a0cc5ed930177bba5715c2df2ebf1b06e69d07db
winx <white00rabbit@gmail.com>
2012-07-25 21:06:10 +0200

    dont reCreateing MainActivity

--- AndiX/src/activity/pack/AndiXActivity.java
+++ AndiX/src/activity/pack/AndiXActivity.java
@@ -1,15 +1,10 @@
 package activity.pack;
 
 import android.app.Activity;
-import android.app.AlertDialog;
-import android.content.DialogInterface;
-import android.net.ConnectivityManager;
-import android.net.NetworkInfo;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.SeekBar;
 import android.widget.SeekBar.OnSeekBarChangeListener;
-import android.widget.Toast;
 
 import com.main.R;
 
@@ -25,40 +20,41 @@ public class AndiXActivity extends Activity implements OnSeekBarChangeListener {
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.main);
-
+		
+		System.out.println("CREATRED!!!");
 		mSeekBar = (SeekBar) findViewById(R.id.seekBar1);
 		mSeekBar.setOnSeekBarChangeListener(this);
 
-		ConnectivityManager connManager = (ConnectivityManager) getSystemService(CONNECTIVITY_SERVICE);
-		NetworkInfo mWifi = connManager
-				.getNetworkInfo(ConnectivityManager.TYPE_WIFI);
+//		ConnectivityManager connManager = (ConnectivityManager) getSystemService(CONNECTIVITY_SERVICE);
+//		NetworkInfo mWifi = connManager
+//				.getNetworkInfo(ConnectivityManager.TYPE_WIFI);
 
 		Bundle bundel = getIntent().getExtras();
 		hostToConnect = (HostAccount) bundel.get("hostToConnect");
 
 		// if (mWifi.isConnected()) {
-		if (true) {
-			new Thread(new Runnable() {
-				public void run() {
-					ssh = new ConnectSSH(hostToConnect,22);
-					System.out.println("IN_THREAD");
-					ssh.connect();
-					System.out.println("OUT_THREAD");
+		new Thread(new Runnable() {
+			public void run() {
+				ssh = new ConnectSSH(hostToConnect, 22);
+				System.out.println("IN_THREAD");
+				ssh.connect();
+				System.out.println("OUT_THREAD");
 
-				}
-			}).start();
-		} else {
-			AlertDialog ad = new AlertDialog.Builder(this).create();
-			ad.setCancelable(true); // This blocks the 'BACK' button
-			ad.setMessage("Please connect to network...");
-			ad.setButton("OK", new DialogInterface.OnClickListener() {
-				@Override
-				public void onClick(DialogInterface dialog, int which) {
-
-				}
-			});
-			ad.show();
-		}
+			}
+		}).start();
+		// } else {
+		// AlertDialog ad = new AlertDialog.Builder(this).create();
+		// ad.setCancelable(true); // This blocks the 'BACK' button
+		// ad.setMessage("Please connect to network...");
+		// ad.setButton("OK", new DialogInterface.OnClickListener() {
+		// @Override
+		// public void onClick(DialogInterface dialog, int which) {
+		// startActivity(new
+		// Intent(android.provider.Settings.ACTION_WIFI_SETTINGS));
+		// }
+		// });
+		// ad.show();
+		// }
 
 	}
 
